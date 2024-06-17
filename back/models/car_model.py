71b753254b8ccfc20592b119e79085e31732d466
Rachel2070 <rachelwe939@gmail.com>
2024-06-17 12:36:35 +0300

    create user model

--- back/models/car_model.py
+++ back/models/car_model.py
@@ -29,8 +29,15 @@ def create_car(new_car):
                 new_car['car_pic'] = 'NONE'
             query = """INSERT INTO cars (model, color, manufacturer, manufacturer_date, license_num, price, num_seats, car_pic) 
                        VALUES (?, ?, ?, ?, ?, ?, ?, ?)"""
-            values = (new_car['model'], new_car['color'], new_car['manufacturer'], new_car['manufacturer_date'],
-                      new_car['license_num'], new_car['price'], new_car['num_seats'], new_car['car_pic'])
+            values = (
+                new_car['model'],
+                new_car['color'],
+                new_car['manufacturer'],
+                new_car['manufacturer_date'],
+                new_car['license_num'],
+                new_car['price'],
+                new_car['num_seats'],
+                new_car['car_pic'])
             cursor.execute(query, values)
             return {'status': 'success', 'message': 'Car added successfully', 'new car': new_car}
         else:
@@ -41,13 +48,26 @@ def update_car(updated_car):
     with pyodbc.connect(connection_str) as connection:
         cursor = connection.cursor()
         if updated_car:
-            if 'car_pic' not in new_car:
-                new_car['car_pic'] = 'NONE'
+            # Ensure car_pic has a default value if not provided
+            if 'car_pic' not in updated_car:
+                updated_car['car_pic'] = 'NONE'
+
             update_query = """UPDATE cars SET model = ?, color = ?, manufacturer = ?, manufacturer_date = ?, license_num = ?, price = ?, num_seats = ?, car_pic = ?
                               WHERE car_id = ?"""
-            values = (updated_car['model'], updated_car['color'], updated_car['manufacturer'], updated_car['manufacturer_date'], updated_car['license_num'], updated_car['price'], updated_car['num_seats'], updated_car['car_id'], updated_car['car_pic'])
+            values = (
+                updated_car['model'],
+                updated_car['color'],
+                updated_car['manufacturer'],
+                updated_car['manufacturer_date'],
+                updated_car['license_num'],
+                updated_car['price'],
+                updated_car['num_seats'],
+                updated_car['car_pic'],
+                updated_car['car_id']
+            )
             cursor.execute(update_query, values)
-            return {'status': 'success', 'message': 'Car updated successfully', 'update car': updated_car}
+            connection.execute()  # Commit the transaction
+            return {'status': 'success', 'message': 'Car updated successfully', 'updated_car': updated_car}
         else:
             return {'error': 'Car is undefined'}
 
@@ -61,15 +81,15 @@ def delete_car(car_id):
 
 
 new_car = {
-    'model': 'Model S',
+    'car_id': 2011,
+    'model': 'Model d',
     'color': 'Red',
     'manufacturer': 2,
     'manufacturer_date': '2023-01-15',
-    'license_num': 'A852C5423',
+    'license_num': 'dtr852C5423',
     'price': 30000,
     'num_seats': 5,
 }
-result = get_car_by_id(2017)
-print(result)
 
-
+# result = update_car(new_car)
+# print(result)
---
+++ back/models/user_model.py
@@ -0,0 +1,87 @@
+import pyodbc
+
+connection_str = """DRIVER={SQL Server};
+        SERVER=MYCOMP;
+        DATABASE=carStore"""
+
+
+def get_all_users():
+    with pyodbc.connect(connection_str) as connection:
+        cursor = connection.cursor()
+        query = "select * from users"
+        cursor.execute((query))
+        return cursor.fetchall()
+
+
+def get_user_by_id(id):
+    with pyodbc.connect(connection_str) as connection:
+        cursor = connection.cursor()
+        query = f"select * from users where user_id = '{id}'"
+        cursor.execute((query))
+        return cursor.fetchone()
+
+
+def create_user(new_user):
+    with pyodbc.connect(connection_str) as connection:
+        cursor = connection.cursor()
+        if new_user:
+            if 'credit_card' not in new_user:
+                new_user['credit_card'] = 'NONE'
+            if 'user_age' not in new_user:
+                new_user['user_age'] = 'NONE'
+            query = """INSERT INTO users (user_name, user_address, user_email, credit_card, user_age) 
+                       VALUES (?, ?, ?, ?, ?)"""
+            values = (
+                new_user['user_name'],
+                new_user['user_address'],
+                new_user['user_email'],
+                new_user['credit_card'],
+                new_user['user_age'])
+            cursor.execute(query, values)
+            return {'status': 'success', 'message': 'User added successfully', 'new user': new_user}
+        else:
+            return {'error': 'User is undefined'}
+
+
+def update_user(updated_user):
+    with pyodbc.connect(connection_str) as connection:
+        cursor = connection.cursor()
+        if updated_user:
+            if 'credit_card' not in updated_user:
+                updated_user['credit_card'] = 'NONE'
+            if 'user_age' not in updated_user:
+                updated_user['user_age'] = 'NONE'
+
+            update_query = """UPDATE users SET user_name = ?, user_address = ?, user_email = ?, credit_card = ?, user_age = ?
+                              WHERE user_id = ?"""
+            values = (
+                updated_user['user_name'],
+                updated_user['user_address'],
+                updated_user['user_email'],
+                updated_user['credit_card'],
+                updated_user['user_age'],
+                updated_user['user_id']
+            )
+            cursor.execute(update_query, values)
+            return {'status': 'success', 'message': 'User updated successfully', 'updated_user': updated_user}
+        else:
+            return {'error': 'User is undefined'}
+
+
+def delete_user(user_id):
+    with pyodbc.connect(connection_str) as connection:
+        cursor = connection.cursor()
+        query = f"delete from users where user_id = '{user_id}'"
+        cursor.execute((query))
+        return {'status': 'success', 'message': 'User deleted successfully'}
+
+new_user = {
+    'user_id': 100,
+    'user_name': 'ddo bbb',
+    'user_address': '1gg3 nain St',
+    'user_email': 'john.doe@example.com',
+    'user_age': 30
+}
+
+# result = get_user_by_id(100)
+# print(result)
